bf612004d4cb5e1ad5e0621dfb4217af7067a089
BamBoos28 <127811854+Trafelselfi05@users.noreply.github.com>
2025-09-05 14:30:51 +0700

    big update
    1. update web.php and Controller
    2. Sidebar and Mobile Dock

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function adminInfo() {
+        return view('admin.admin-info');
+    }
+
+    public function dashboard() {
+        return view('admin.dashboard');
+    }
+
+    public function project() {
+        return view('admin.project');
+    }
+
+    public function createProject() {
+        return view('admin.create-project');
+    }
+
+    public function task() {
+        return view('admin.task');
+    }
+
+    public function taskDetail() {
+        return view('admin.task-detail');
+    }
+
+    public function activity() {
+        return view('admin.activity');
+    }
+
+    public function administration() {
+        return view('admin.administration');
+    }
+
+    public function profileAdmin() {
+        return view('admin.profile-admin');
+    }
+
+    public function userAccount() {
+        return view('admin.user-account');
+    }
+
+    public function userDetail() {
+        return view('admin.user-detail');
+    }
+}
--- resources/views/user/project-user.blade.php
+++ resources/views/user/project-user.blade.php
@@ -15,7 +15,7 @@
     </div>
 
             <!-- Create Project Button -->
-            <a href="{{ url('user/create-project-user') }}"
+            <a href="{{ route("create-project-user") }}"
                 class="flex items-center gap-2.5 bg-black hover:bg-gray-800 rounded-[10px] px-3 py-3.5 transition-colors">
                 <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 5V19M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round" />
